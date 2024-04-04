ecd8afdc76fd818ecaefd3b594c0d632e3283932
Elber C. da França <ellberfranca86@gmail.com>
2024-04-04 14:09:55 -0300

    Resolvida questão 6 com auxilio do gabarito

--- loops.py
+++ loops.py
@@ -12,7 +12,14 @@ imprimir_contagem()
 def contagem_usuario():
     digito_user = int(input("Digite um numero: "))
 
+#exemplo for
     for i in range(digito_user + 1):
         print(i)
+
+#exemplo while
+    contador = 0
+    while(contador <= digito_user):
+        print(contador)
+        contador = contador + 1
 
 contagem_usuario()
