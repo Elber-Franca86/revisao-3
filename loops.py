d588372da1e4766919a8458a8fd69af8ce3a1cb2
Elber C. da França <ellberfranca86@gmail.com>
2024-04-04 14:06:47 -0300

    Resolvida com auxilio do gabarito

--- loops.py
+++ loops.py
@@ -3,8 +3,16 @@ def imprimir_contagem():
     #    print(i)
 
     contador = 0
-    while(contador < 11):
+    while(contador < 21):
         print(contador)
         contador = contador +1
 
 imprimir_contagem()
+
+def contagem_usuario():
+    digito_user = int(input("Digite um numero: "))
+
+    for i in range(digito_user + 1):
+        print(i)
+
+contagem_usuario()
