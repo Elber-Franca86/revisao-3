1c7f31b75890a97dd04c3d219cb5d3f7c4e039f9
Elber C. da França <ellberfranca86@gmail.com>
2024-04-04 14:53:30 -0300

    Resolve questão 8

--- loops.py
+++ loops.py
@@ -39,3 +39,20 @@ def tabuada_de_dois():
         contador = contador + 1    
 
 tabuada_de_dois()
+
+
+def tabuada_multiplicar():
+    dig_num = int(input("Digite um numero inteiro: "))
+    #Ex. for
+#for i in range(1, 11):
+    #    multplicar = dig_num * i
+   #     print(dig_num, "x" ,i, "=", multplicar)
+
+    contador = 1
+    while(contador < 11):
+        multiplicar = dig_num * contador
+        print(dig_num, "x" , contador, "=", multiplicar)
+        contador = contador + 1
+
+
+tabuada_multiplicar()
