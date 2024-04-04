a42bbdd33db4e5cba5a8774f5f5d84cba07ef6f9
Elber C. da França <ellberfranca86@gmail.com>
2024-04-03 21:22:16 -0300

    Resolve questão 5

---
+++ loops.py
@@ -0,0 +1,10 @@
+def imprimir_contagem():
+   # for i in range(21):
+    #    print(i)
+
+    contador = 0
+    while(contador < 11):
+        print(contador)
+        contador = contador +1
+
+imprimir_contagem()
